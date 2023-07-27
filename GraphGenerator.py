ff2d35c6ad77bb53096bc162939d36f5656e2824
EmicoBinsfinder <egheosao@gmail.com>
2023-07-27 14:44:14 +0100

    Update Test

--- GraphGenerator.py
+++ GraphGenerator.py
@@ -196,6 +196,8 @@ print(Y_labels)
 
 data_list = create_pytorch_geometric_graph_data_list_from_smiles_and_labels(Smiles_list, Y_labels)
 
+tu_dataset = torch_geometric.datasets.TUDataset(root=DATASET_PATH, name="MUTAG")
+
 ################## DEFINE MODEL ARCHITECTURE ######################
 
 ## Defining different GNN layers from the Torch Geometric library for use later on 
@@ -344,39 +346,38 @@ class GraphLevelGNN(pl.LightningModule):
 
 ################## DEFINE TRAINING LOOP ###############################
 
-def train_graph_model(model_name, **model_kwargs):
-    pl.seed_everything(42)
+# def train_graph_model(model_name, **model_kwargs):
+#     pl.seed_everything(42)
 
-    # Create a PyTorch Lightning trainer with the generation callback
-    root_dir = os.path.join(CHECKPOINT_PATH, "GraphLevel" + model_name)
-    os.makedirs(root_dir, exist_ok=True)
-    trainer = pl.Trainer(default_root_dir=root_dir,
-                         callbacks=[ModelCheckpoint(save_weights_only=True, mode="max", monitor="val_acc")],
-                         accelerator="gpu" if str(device).startswith("cuda") else "cpu",
-                         devices=1,
-                         max_epochs=500,
-                         enable_progress_bar=False)
-    trainer.logger._default_hp_metric = None # Optional logging argument that we don't need
+#     # Create a PyTorch Lightning trainer with the generation callback
+#     root_dir = os.path.join(CHECKPOINT_PATH, "GraphLevel" + model_name)
+#     os.makedirs(root_dir, exist_ok=True)
+#     trainer = pl.Trainer(default_root_dir=root_dir,
+#                          callbacks=[ModelCheckpoint(save_weights_only=True, mode="max", monitor="val_acc")],
+#                          accelerator="gpu" if str(device).startswith("cuda") else "cpu",
+#                          devices=1,
+#                          max_epochs=500,
+#                          enable_progress_bar=False)
 
-    # Check whether pretrained model exists. If yes, load it and skip training
-    pretrained_filename = os.path.join(CHECKPOINT_PATH, f"GraphLevel{model_name}.ckpt")
-    if os.path.isfile(pretrained_filename):
-        print("Found pretrained model, loading...")
-        model = GraphLevelGNN.load_from_checkpoint(pretrained_filename)
-    else:
-        pl.seed_everything(42)
-        model = GraphLevelGNN(c_in=tu_dataset.num_node_features,
-                              c_out=1 if tu_dataset.num_classes==2 else tu_dataset.num_classes,
-                              **model_kwargs)
-        trainer.fit(model, graph_train_loader, graph_val_loader)
-        model = GraphLevelGNN.load_from_checkpoint(trainer.checkpoint_callback.best_model_path)
-    # Test best model on validation and test set
-    train_result = trainer.test(model, graph_train_loader, verbose=False)
-    test_result = trainer.test(model, graph_test_loader, verbose=False)
-    result = {"test": test_result[0]['test_acc'], "train": train_result[0]['test_acc']}
-    return model, result
+#     # Check whether pretrained model exists. If yes, load it and skip training
+#     pretrained_filename = os.path.join(CHECKPOINT_PATH, f"GraphLevel{model_name}.ckpt")
+#     if os.path.isfile(pretrained_filename):
+#         print("Found pretrained model, loading...")
+#         model = GraphLevelGNN.load_from_checkpoint(pretrained_filename)
+#     else:
+#         pl.seed_everything(42)
+#         model = GraphLevelGNN(c_in=tu_dataset.num_node_features,
+#                               c_out=1 if tu_dataset.num_classes==2 else tu_dataset.num_classes,
+#                               **model_kwargs)
+#         trainer.fit(model, graph_train_loader, graph_val_loader)
+#         model = GraphLevelGNN.load_from_checkpoint(trainer.checkpoint_callback.best_model_path)
+#     # Test best model on validation and test set
+#     train_result = trainer.test(model, graph_train_loader, verbose=False)
+#     test_result = trainer.test(model, graph_test_loader, verbose=False)
+#     result = {"test": test_result[0]['test_acc'], "train": train_result[0]['test_acc']}
+#     return model, result
 
-# model, result = train_graph_model(model_name="GraphConv",
+# model, result = train_graph_model(model_name="Graph_Viscosity_Index_Predictor",
 #                                        c_hidden=256,
 #                                        layer_name="GraphConv",
 #                                        num_layers=3,
